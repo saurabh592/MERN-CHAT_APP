0472507fba323c668b9901f69a31877dc9e1b1db
saurabh592 <shubham.pathak9323@gmail.com>
2023-08-18 14:14:18 +0530

    update login and signup and cloudniary

--- backend/models/userModels.js
+++ backend/models/userModels.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require("bcryptjs")
 
-const userModel = mongoose.Schema(
+const userSchema = mongoose.Schema(
     {
         name: {
             type: String,
@@ -8,7 +9,8 @@ const userModel = mongoose.Schema(
         },
         email: {
             type: String,
-            require: true
+            require: true,
+            unique: true
         },
         password: {
             type: String,
@@ -16,7 +18,6 @@ const userModel = mongoose.Schema(
         },
         pic: {
             type: String,
-            require: true,
             default: "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
         },
         // isAdmin: {
@@ -30,7 +31,18 @@ const userModel = mongoose.Schema(
     }
 
 );
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password)
+}
 
-const User = mongoose.model("User", userModel);
+userSchema.pre('save', async function (next) {
+    if (!this.isModified) {
+        next()
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt)
+})
+
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
--- backend/server.js
+++ backend/server.js
@@ -3,23 +3,25 @@ const express = require("express");
 const { chats } = require("./data/data");
 const connectDB = require('./config/db');
 const colors = require('colors')
+const userRoutes = require('./routes/userRoutes')
+const { notFound, errorHandler } = require('./middleware/errorMiddleware')
 
 dotenv.config();
 connectDB();
 const app = express();
+app.use(express.json());
 
 
 app.get('/', (req, res) => {
     res.send("api is running")
 });
-app.get('/api/chat', (req, res) => {
-    res.send(chats)
-});
-app.get('/api/chat/:id', (req, res) => {
-    const singleChat = chats.find((c) => c._id == req.params.id);
-    res.send(singleChat);
-    console.log(singleChat);
-});
+app.use('/api/user', userRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
+
+
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`server has started ${PORT}`.yellow.bold))
